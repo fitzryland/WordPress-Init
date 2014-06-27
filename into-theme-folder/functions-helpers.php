68fed7be57a09f06074a07176b16df4ea23b42c0
Fitz Ryland <fitzryland@gmail.com>
2014-06-27 08:50:40 -0700

    added acf theme integration and documented

--- into-theme-folder/functions-helpers.php
+++ into-theme-folder/functions-helpers.php
@@ -34,4 +34,56 @@ function acf_link($options) { // link_text, link_destination, class
   $html .= '>' . $linkText . '</a>';
   return $html;
 }
+
+function acf_in_theme() {
+  // ADD ACF
+  if( ! class_exists('Acf') ) {
+    if ( ! defined( 'FITZ_ENVIRONMENT' ) || FITZ_ENVIRONMENT !== 'local' ) {
+      // TODO!!!! define( 'ACF_LITE' , true );
+    }
+    include_once('inc/advanced-custom-fields/acf.php' );
+  }
+  // test change
+
+  // ADD REPEATER ADD ON
+  if( !function_exists('acf_register_repeater_field') ):
+    // add action to include field
+    add_action('acf/register_fields', 'acf_register_repeater_field');
+    function acf_register_repeater_field() {
+      include_once('inc/acf-repeater/repeater.php');
+    }
+  endif; // class_exists check
+
+
+  // ADD OPTIONS PAGE ADD ON
+  if( ! class_exists('acf_options_page_plugin') ) {
+    include_once('inc/acf-options-page/acf-options-page.php');
+  }
+
+  if ( ! defined( 'FITZ_ENVIRONMENT' ) || FITZ_ENVIRONMENT !== 'local' ) {
+    require_once('inc/acf-export.php');
+  }
+
+
+}
+//
+// Moving ACF into theme
+// 1) Move ACF and addons into the inc folder in theme
+// 2) Ensure that FITZ_ENVIRONMENT is set correctly
+//    2.1) define('FITZ_ENVIRONMENT', 'local');
+// 3) ensure that the fields are exported to inc/acf-export.php
+// 4) uncomment acf_in_theme()
+// 5) migrate to testing
+// 6) delete all fields via the GUI on testing
+// 7) uncomment the ACF_LITE activation line and push to testing
+// acf_in_theme();
+
+
+// change name of options panel
+if( function_exists('acf_set_options_page_menu') ) {
+  acf_set_options_page_menu( __('Theme Settings') );
+}
+
+
+
 ?>
